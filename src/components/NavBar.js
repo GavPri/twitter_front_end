1e66b46a04cf853e545bdb29b94176bb0e9517d3
德华 刘 <gavinprior5@gmail.com>
2023-11-10 23:16:45 +0000

    update color theme

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <header className="w-full h-24 flex p-4 items-center text-text-color bg-background-color">
+      <nav className="w-full flex items-center justify-between">
+        <h1>Twitter Clone</h1>
+        <ul className="flex">
+          <li className="px-4">Home</li>
+          <li className="px-4">Sign In</li>
+          <li className="px-4">Sign Up</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default NavBar
+export default NavBar;
