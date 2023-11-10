b41463da365cbaa5ff2061af5c4a4333a79b9cf9
德华 刘 <gavinprior5@gmail.com>
2023-11-10 23:30:42 +0000

    style mobile dropdown

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
     <header className="w-full h-24 flex p-4 items-center text-text-color bg-background-color">
       <nav className="w-full flex items-center justify-between">
         <h1>Twitter Clone</h1>
-        <ul className="flex">
+        <ul className="flex hidden">
           <li className="px-4">Home</li>
           <li className="px-4">Sign In</li>
           <li className="px-4">Sign Up</li>
@@ -15,11 +15,11 @@ const NavBar = () => {
       <div>
         <AiOutlineMenu size={20} />
       </div>
-      <nav>
-        <ul>
-          <li className="px-4">Home</li>
-          <li className="px-4">Sign In</li>
-          <li className="px-4">Sign Up</li>
+      <nav className="mt-24 fixed right-0 top-0 w-[60%] h-full border-r-accent-color bg-tweet-container-background">
+        <ul className="uppercase">
+          <li className="p-4 border-b border-tweet-border-color">Home</li>
+          <li className="p-4 border-b border-tweet-border-color">Sign In</li>
+          <li className="p-4">Sign Up</li>
         </ul>
       </nav>
     </header>
