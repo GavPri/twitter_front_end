fb0479e9b0c2298924197e4342325c0fd448482c
德华 刘 <gavinprior5@gmail.com>
2023-11-10 23:01:33 +0000

    install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "white-c": "#ffffff",
+      "purple-c": "#3f3cbb",
+      "midnight-c": "#121063",
+      "metal-c": "#565584",
+      "tahiti-c": "#3ab7bf",
+      "silver-c": "#ecebff",
+      "bubble-gum-c'": "#ff77e9",
+      "bermuda-c": "#78dcca",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
