c0a5288bfd4c93dc4315b845d67236f1fc2cc2a6
德华 刘 <gavinprior5@gmail.com>
2023-11-11 07:24:22 +0000

    current user contect added to app.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import axios from "axios";
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
 
 function App() {
   // get currently logged in user
@@ -25,20 +28,24 @@ function App() {
     handleMount();
   }, []);
   return (
-    <div className="App">
-      <NavBar />
-      <div className="mt-12 bg-background-color h-[calc(100vh-48px)] flex items-center justify-center">
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => <h1 className="text-text-color">HomePage</h1>}
-          />
-          <Route exact path="/signin" render={() => <SignInForm />} />
-          <Route exact path="/signup" render={() => <SignUpForm />} />
-        </Switch>
-      </div>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        <div className="App">
+          <NavBar />
+          <div className="mt-12 bg-background-color h-[calc(100vh-48px)] flex items-center justify-center">
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={() => <h1 className="text-text-color">HomePage</h1>}
+              />
+              <Route exact path="/signin" render={() => <SignInForm />} />
+              <Route exact path="/signup" render={() => <SignUpForm />} />
+            </Switch>
+          </div>
+        </div>
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
