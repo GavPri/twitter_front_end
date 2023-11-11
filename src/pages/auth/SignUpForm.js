65cfe3cd7b62320d46e958acc554386c3e972340
德华 刘 <gavinprior5@gmail.com>
2023-11-11 04:56:05 +0000

    create handle change function

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -10,6 +10,13 @@ const SignUpForm = () => {
   });
   // Destructure the signup data
   const { username, password1, password2 } = signUpData;
+  // Handle change function for form inputs
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
   return (
     <div className="overflow-y-hidden mt-12 mx-auto w-full h-[calc(100vh-24px)] bg-background-color flex justify-center items-center">
       <div className="text-text-color w-[80%] py-6 px-4 border border-tweet-border-color rounded shadow-lg h-[50%] md:w-[50%]">
@@ -32,10 +39,11 @@ const SignUpForm = () => {
               id="username"
               name="username"
               value={username}
+              onChange={handleChange}
               required
               placeholder="Username"
               className="w-full border border-tweet-border-color px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-accent-color-300 focus:ring
-            focus:ring-accent-color-300 mb-6"
+            focus:ring-accent-color-300 mb-6 text-background-color"
             />
             <label htmlFor="password1" className="hidden">
               Username:
@@ -45,10 +53,11 @@ const SignUpForm = () => {
               id="password"
               name="password1"
               value={password1}
+              onChange={handleChange}
               required
               placeholder="Enter password"
               className="w-full border border-tweet-border-color px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-accent-color-300 focus:ring
-            focus:ring-accent-color-300 mb-6"
+            focus:ring-accent-color-300 mb-6 text-background-color"
             />
             <label htmlFor="password2" className="hidden">
               Username:
@@ -58,10 +67,11 @@ const SignUpForm = () => {
               id="password2"
               name="password2"
               value={password2}
+              onChange={handleChange}
               required
               placeholder="Confirm Your Password"
               className="w-full border border-tweet-border-color px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-accent-color-300 focus:ring
-            focus:ring-accent-color-300 mb-6"
+            focus:ring-accent-color-300 mb-6 text-background-color"
             />
             <button
               type="submit"
