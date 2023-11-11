29c4510f36990ec45b419bbbb55f4d85ff22d394
德华 刘 <gavinprior5@gmail.com>
2023-11-11 02:38:02 +0000

    add pages & auth folder, signup js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
 
 function App() {
   return (
