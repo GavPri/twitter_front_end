3fc6352c04e0bb6d67a05fa9b367eee2ac101ab4
德华 刘 <gavinprior5@gmail.com>
2023-11-11 02:35:56 +0000

    add axios with credentials

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -2,3 +2,4 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://twitter-ci-4eb0a43581b7.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
