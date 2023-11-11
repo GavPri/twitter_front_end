ce2b3088d6a32fa3179386df7d770c2681693bbc
德华 刘 <gavinprior5@gmail.com>
2023-11-11 06:36:54 +0000

    remove password two and link from signin

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -21,18 +21,21 @@ const SignInForm = () => {
   };
   // error storage
   const [errors, setErrors] = useState({});
+  //  Handle form submission
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      console.log("clicked");
+      await axios.post("/dj-rest-auth/login", signUpData);
+      history.push("/signin");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
   return (
     <div className="overflow-y-hidden mt-12 mx-auto w-full h-[calc(100vh-24px)] bg-background-color flex justify-center items-center">
       <div className="text-text-color w-[80%] py-6 px-4 border border-tweet-border-color rounded shadow-lg h-[50%] md:w-[50%]">
-        <h2 className="text-2xl pb-2">Sign Up!</h2>
-        <p className="pb-4 w-full">
-          Already have an account?{" "}
-          <NavLink to="/signin">
-            <span className="text-link-color  hover:text-accent-color">
-              Sign In!
-            </span>
-          </NavLink>
-        </p>
+        <h2 className="text-2xl pb-2">Sign In!</h2>
         <div>
           <form onSubmit={handleSubmit}>
             <label htmlFor="username" className="hidden">
@@ -69,25 +72,6 @@ const SignInForm = () => {
             focus:ring-accent-color-300 mb-6 text-background-color"
             />
             {errors.password1?.map((message, id) => (
-              <div className="w-full h-8 flex justify-center items-center text-rose mb-1 border border-warning px-3 py-2 rounded rounded-md">
-                <p className="text-md font-bold text-warning">{message}</p>
-              </div>
-            ))}
-            <label htmlFor="password2" className="hidden">
-              Confirm password:
-            </label>
-            <input
-              type="password"
-              id="password2"
-              name="password2"
-              value={password2}
-              onChange={handleChange}
-              // required
-              placeholder="Confirm Your Password"
-              className="w-full border border-tweet-border-color px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-accent-color-300 focus:ring
-            focus:ring-accent-color-300 mb-6 text-background-color"
-            />
-            {errors.password2?.map((message, id) => (
               <div className="w-full h-8 flex justify-center items-center text-rose mb-1 border border-warning px-3 py-2 rounded rounded-md">
                 <p className="text-md font-bold text-warning">{message}</p>
               </div>
