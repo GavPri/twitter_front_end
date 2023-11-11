cb8aa68c8a1228f3e8116b947c30aea89858fac9
德华 刘 <gavinprior5@gmail.com>
2023-11-11 17:32:11 +0000

    create axios instances for interceptors

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL = "https://twitter-ci-4eb0a43581b7.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+// Axios instance to attach interceptors
+export const axiosReq = axios.create();
+export const axiosRes = axios.create()
