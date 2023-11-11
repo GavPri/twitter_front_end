3b41d2c47881ca490d2161a36f6bd5481a0a57a2
德华 刘 <gavinprior5@gmail.com>
2023-11-11 05:04:23 +0000

    set push on form submit

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -25,6 +25,7 @@ const SignUpForm = () => {
     event.preventDefault();
     try{
       await axios.post('/dj-rest-auth/registration/', signUpData)
+      history.push('/signin')
     }catch(err){
 
     }
