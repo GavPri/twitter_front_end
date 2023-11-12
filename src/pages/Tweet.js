8de196cfdefd3d2053a50d9f69af7d5cc020b0ae
德华 刘 <gavinprior5@gmail.com>
2023-11-12 23:46:48 +0000

    add props to tweet page

--- src/pages/Tweet.js
+++ src/pages/Tweet.js
@@ -1,6 +1,17 @@
 import React from "react";
 
 const Tweet = (props) => {
+  const {
+    id,
+    owner,
+    account_id,
+    account_image,
+    comments_count,
+    likes_count,
+    like_id,
+    content,
+    image,
+  } = props;
   return <div>PlaceHolder Texts</div>;
 };
 
