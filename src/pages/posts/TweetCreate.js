24153d496901b41d47bfc9e82b123b57abbe24ae
德华 刘 <gavinprior5@gmail.com>
2023-12-03 20:57:08 +0000

    made changes to tweet create form

--- src/pages/posts/TweetCreate.js
+++ src/pages/posts/TweetCreate.js
@@ -1,27 +1,32 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
-const TweetCreate = () => {
+function TweetCreate() {
+  // ---- store errors
+  const [errors, setErrors] = useState({});
+
+  //  ---- deconstruct data
   const [tweetData, setTweetData] = useState({
     content: "",
     image: "",
   });
-  const [errors, setErrors] = useState({});
   const { content, image } = tweetData;
-  // Handle Change
+
+  //  ---- useRef for image upload
+  const imageInput = useRef(null);
+  // ---- history to redirect user
+  const history = useHistory();
+
+  // ---- HandleChange
   const handleChange = (event) => {
     setTweetData({
       ...tweetData,
       [event.target.name]: event.target.value,
     });
   };
-
-  const history = useHistory();
-  const imageInput = useRef(null);
-
-  // image change
-  const handleImageChange = (event) => {
+  // ---- Handle image change
+  const handleChangeImage = (event) => {
     if (event.target.files.length) {
       URL.revokeObjectURL(image);
       setTweetData({
@@ -30,15 +35,15 @@ const TweetCreate = () => {
       });
     }
   };
-  // handle submit
+  // ---- Handle Submit
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData();
-
     formData.append("content", content);
-    formData.append("image", image);
+    formData.append("image", imageInput.current.files[0]);
+
     try {
-      const { data } = await axiosReq.post("/tweets", formData);
+      const { data } = await axiosReq.post("/tweets/", formData);
       history.push(`/feed/${data.id}`);
     } catch (err) {
       console.log(err);
@@ -47,6 +52,7 @@ const TweetCreate = () => {
       }
     }
   };
+
   return (
     <div className="w-full h-[90%] flex justify-center items-center">
       {/* text area  */}
@@ -70,48 +76,58 @@ const TweetCreate = () => {
           className="w-full border border-tweet-border-color px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-accent-color-300 focus:ring
             focus:ring-accent-color-300 mb-6 text-background-color resize-none h-40"
         />
-        {errors.content?.map((message, idx) => (
-          <div className="w-full h-8 flex justify-center items-center text-rose mb-1 border border-warning px-3 py-2 rounded rounded-md">
+        {errors?.content?.map((message, idx) => (
+          <div
+            key={idx}
+            className="w-full h-8 flex justify-center items-center text-rose mb-1 border border-warning px-3 py-2 rounded rounded-md"
+          >
             <p className="text-md font-bold text-warning">{message}</p>
           </div>
         ))}
         {/* Image upload */}
         <label
-          htmlFor="tweetcreatefile"
+          htmlFor="tweet-image"
           className="text-3xl text-text-color mb-6 justify-start block"
         >
           Upload Photo
         </label>
         <div className="rounded overflow-hidden h-50 w-50 flex items-start">
           {image ? (
-            <figure className="rounded w-full h-full">
-              <img src={image} alt="" className="w-24 h-24 object-cover mb-6" />
-              <button
-                onClick={handleImageChange}
-                className=" bottom-0 left-0 w-full mb-6 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-md font-medium text-text-color bg-link-color  focus:outline-none focus:ring-2 focus:ring-offset-2"
-              >
-                Remove Photo
-              </button>
-            </figure>
+            <>
+              <figure className="rounded w-full h-full">
+                <img
+                  src={image}
+                  alt=""
+                  className="w-24 h-24 object-cover mb-6"
+                />
+                <button
+                  onClick={handleChangeImage}
+                  className=" bottom-0 left-0 w-full mb-6 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-md font-medium text-text-color bg-link-color  focus:outline-none focus:ring-2 focus:ring-offset-2"
+                >
+                  Change Photo
+                </button>
+              </figure>
+            </>
           ) : (
-            <div>
+            <>
               <input
-                ref={imageInput}
                 type="file"
-                id="Upload Photo"
-                name="image"
-                value={image}
-                onChange={handleImageChange}
+                id="image-upload"
                 accept="image/*"
+                onChange={handleChangeImage}
+                ref={imageInput}
                 className="file:w-1/3 file:border file:border-tweet-border-color file:px-3 file:py-2 file:rounded-lg file:shadow-sm file:focus:outline-none focus:border-accent-color-300 focus:ring
             file:focus:ring-accent-color-300 file:mb-6 file:text-background-color file:resize-none file:h-10 hover:cursor-pointer"
               />
-              {errors.image?.map((message, id) => (
-                <div className="w-full h-8 flex justify-center items-center text-rose mb-1 border border-warning px-3 py-2 rounded rounded-md">
+              {errors?.image?.map((message, idx) => (
+                <div
+                  key={idx}
+                  className="w-full h-8 flex justify-center items-center text-rose mb-1 border border-warning px-3 py-2 rounded rounded-md"
+                >
                   <p className="text-md font-bold text-warning">{message}</p>
                 </div>
               ))}
-            </div>
+            </>
           )}
         </div>
 
@@ -122,7 +138,10 @@ const TweetCreate = () => {
           >
             Cancel
           </button>
-          <button className="w-1/3 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-md font-medium text-text-color bg-link-color  hover:scale-125 hover:ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2">
+          <button
+            type="submit"
+            className="w-1/3 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-md font-medium text-text-color bg-link-color  hover:scale-125 hover:ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2"
+          >
             Create
           </button>
         </div>
@@ -130,6 +149,6 @@ const TweetCreate = () => {
       </form>
     </div>
   );
-};
+}
 
 export default TweetCreate;
