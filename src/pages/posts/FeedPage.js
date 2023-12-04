ddf831e43228ea748435f454257bf894863e640b
德华 刘 <gavinprior5@gmail.com>
2023-12-04 22:45:50 +0000

    destructuring & use effect in feedpage

--- src/pages/posts/FeedPage.js
+++ src/pages/posts/FeedPage.js
@@ -1,6 +1,27 @@
-import React from "react";
+import { AxiosHeaders } from "axios";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import axiosReq from "../../api/axiosDefaults"
 
-const FeedPage = () => {
+const FeedPage = ({ message, filter=""}) => {
+
+    const[tweets, setTweets] = useState({ results: []});
+    const[hasLoaded, setHasLoaded] = useState(false)
+    const {pathname} = useLocation()
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const {data} = await axiosReq.get(`tweets/?${filter}`)
+                setTweets(data)
+                setHasLoaded(true)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        setHasLoaded(false)
+        fetchPosts()
+    }, [filter, pathname])
   return <h1 className="text-text-color">Hello, this is the feed page!</h1>;
 };
 
