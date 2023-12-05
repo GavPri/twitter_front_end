13b3ecc75c693447ab41fdf4f91106dbc3672a9e
德华 刘 <gavinprior5@gmail.com>
2023-12-05 18:15:27 +0000

    install and boilerplate for infinite scroll

--- src/pages/posts/FeedPage.js
+++ src/pages/posts/FeedPage.js
@@ -4,6 +4,7 @@ import Tweet from "./Tweet";
 import { axiosReq } from "../../api/axiosDefaults";
 import { Form } from "react-bootstrap";
 import LoadingBar from "react-top-loading-bar";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const FeedPage = ({ message, filter = "" }) => {
   const [tweets, setTweets] = useState({ results: [] });
@@ -36,13 +37,12 @@ const FeedPage = ({ message, filter = "" }) => {
 
   return (
     <div>
-      <div className="fixed top-0 left-0 h-4 w-full z-50">
+      <div className="fixed top-0 left-0 h-6 w-full z-50">
         <LoadingBar
           height={3}
-          color="#e11d48" // Set the color to bright blue (you can use any other color code)
+          color="#e11d48"
           progress={loadingBarProgress}
           onLoaderFinished={() => setLoadingBarProgress(0)}
-          // className="fixed top-0 left-0 h-[3px] w-full z-50"
         />
       </div>
       <Form onSubmit={(event) => event.preventDefault()} className="mt-4 ">
@@ -60,9 +60,24 @@ const FeedPage = ({ message, filter = "" }) => {
         {hasLoaded ? (
           <>
             {tweets.results.length ? (
-              tweets.results.map((tweet) => (
-                <Tweet key={tweet.id} {...tweet} setTweets={setTweets} />
-              ))
+              <InfiniteScroll
+                children={tweets.results.map((tweet) => (
+                  <Tweet key={tweet.id} {...tweet} setTweets={setTweets} />
+                ))}
+                dataLength={tweets.results.length}
+                loader={
+                  <div className="fixed top-0 left-0 h-6 w-full z-50">
+                    <LoadingBar
+                      height={3}
+                      color="#e11d48"
+                      progress={loadingBarProgress}
+                      onLoaderFinished={() => setLoadingBarProgress(0)}
+                    />
+                  </div>
+                }
+                hasMore={!!tweets.next}
+                next={() => {}}
+              />
             ) : (
               <div className="p-6 bg-link-color flex flex-col w-[80%] items-center justify-center rounded-md h-fit">
                 <p className="text-text-color text-md">{message}</p>
