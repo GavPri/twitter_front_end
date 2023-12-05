925b17e317bc57f2cf19a4e72a656fe4bb14e2b5
德华 刘 <gavinprior5@gmail.com>
2023-12-05 19:40:31 +0000

    create popular profiles page

---
+++ src/pages/profiles/PopularProfiles.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PopularProfiles = () => {
+  return (
+    <div>PopularProfiles
+        <p className='text-text-color text-lg'>Most Followed Profiles</p>
+    </div>
+  )
+}
+
+export default PopularProfiles
