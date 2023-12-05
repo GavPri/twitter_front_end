a57d2804ae137c901eee2db86a2a9f6ceaf937d5
德华 刘 <gavinprior5@gmail.com>
2023-12-05 21:45:10 +0000

    add mobile logic to popular profiles

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -30,20 +30,47 @@ const PopularProfiles = () => {
 
     handleMount();
   }, [currentUser]);
+
+  //   ----- Is mobile
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    // clean up function
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="mt-4 rounded-md flex bg-tweet-container-background border-2 border-tweet-border-color md:flex-col md:fixed md:right-0 md:top-20  md:items-start   md:mt-0 md:w-fit">
+    <div className="mt-4 rounded-md bg-tweet-container-background border-2 border-tweet-border-color md:flex-col md:fixed md:right-0 md:top-20  md:items-start   md:mt-0 md:w-fit">
       <p className="text-text-color text-lg w-full md:p-2">Popular Profiles</p>
-      {popularAccounts.results.map((account) => (
-        <NavLink to="/" className="md:flex md:flex-row-reverse md:mb-4 md:ml-2">
-          <p
-            className="md:ml-2 text-text-color hover:cursor-pointer hover:text-link-color"
-            key={account.id}
+      {popularAccounts.results
+        .slice(0, isMobile ? 5 : undefined)
+        .map((account) => (
+          <NavLink
+            to="/"
+            className="md:flex md:flex-row-reverse md:mb-4 md:ml-2"
           >
-            {account.owner}
-          </p>
-          <Avatar src={account.image} className="hidden md:block" height={30} />
-        </NavLink>
-      ))}
+            <p
+              className="md:ml-2 text-text-color hover:cursor-pointer hover:text-link-color"
+              key={account.id}
+            >
+              {account.owner}
+            </p>
+            <Avatar
+              src={account.image}
+              className="hidden md:block"
+              height={30}
+            />
+          </NavLink>
+        ))}
     </div>
   );
 };
