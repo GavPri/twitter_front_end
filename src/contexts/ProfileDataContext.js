587ca5a916f9a468fa351da00df757cf84c61e01
德华 刘 <gavinprior5@gmail.com>
2023-12-06 15:09:10 +0000

    add account data logic from popular accounts

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,14 +1,39 @@
 import { createContext } from "react";
 
 // ----- Create Context Objects
-export const ProfileDataContext = createContext()
+export const ProfileDataContext = createContext();
 export const setProfileDataContext = createContext();
 
 // ---- Custom hooks
-export const useProfileData = () => createContext(ProfileDataContext)
+export const useProfileData = () => createContext(ProfileDataContext);
 export const useSetProfileData = () => createContext(setProfileDataContext);
 
 // ---- export function component
-export const ProfileDataProvider = ({children}) => {
-    
-}
+export const ProfileDataProvider = ({ children }) => {
+  const [accountData, setAccountData] = useState({
+    pageAccount: { results: [] },
+    popularAccounts: { results: [] },
+  });
+  const { popularAccounts } = accountData;
+
+  const currentUser = useCurrentUser();
+
+  // request for profiles
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/accounts/?ordering=-followers-count"
+        );
+        setAccountData((prevState) => ({
+          ...prevState,
+          popularAccounts: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+};
