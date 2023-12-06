a7d92d3be9b490536459c55e39554f92eb5aad04
德华 刘 <gavinprior5@gmail.com>
2023-12-06 17:02:13 +0000

    create useParams

--- src/pages/accounts/AccountPage.js
+++ src/pages/accounts/AccountPage.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
-import {useCurrentUser } from '../../contexts/CurrentUserContext'
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useParams } from "react-router";
 
 const AccountPage = () => {
   const [hasLoaded, setHasLoaded] = useState(false);
   const currentUser = useCurrentUser();
+  // extarct id from url
+  const { id } = useParams();
 
   useEffect(() => {
-    setHasLoaded(true)
-  })
+    setHasLoaded(true);
+  });
   return (
     <div className="mt-16 h-24 bg-profile-background w-fit p-4 border-2 border-tweet-border-color ">
       <div>{/* Avatar */}</div>
