34b41af0fde21877196f973616458e330a782db8
德华 刘 <gavinprior5@gmail.com>
2023-12-06 17:29:55 +0000

    display username

--- src/pages/accounts/AccountPage.js
+++ src/pages/accounts/AccountPage.js
@@ -39,11 +39,12 @@ const AccountPage = () => {
   return (
     <div className="mt-16 h-24 bg-profile-background w-fit p-4 border-2 border-tweet-border-color ">
       <div>
-        {" "}
         <Image src={account?.image} className="rounded-full" />
       </div>
       <div>
-        <div>{/* user info */}</div>
+        <div>
+          <h3>{account?.owner}</h3>
+        </div>
         <div>{/* follow button  or edit profile button*/}</div>
       </div>
     </div>
