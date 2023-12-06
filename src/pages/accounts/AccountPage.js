271c63ad2d1bfddd90e18ca1af20753330d8d39f
德华 刘 <gavinprior5@gmail.com>
2023-12-06 16:24:49 +0000

    boilerplate for account page

--- src/pages/accounts/AccountPage.js
+++ src/pages/accounts/AccountPage.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const AccountPage = () => {
   return (
-    <div className="mt-16 h-24 bg-profile-background w-fit p-4 border-2 border-tweet-border-color">
-      <h1 className="text-lg text-profile-text-color">
-        Welcome to the account page!
-      </h1>
+    <div className="mt-16 h-24 bg-profile-background w-fit p-4 border-2 border-tweet-border-color ">
+      <div>{/* Avatar */}</div>
+      <div>
+        <div>{/* user info */}</div>
+        <div>{/* follow button  or edit profile button*/}</div>
+      </div>
     </div>
   );
 };
