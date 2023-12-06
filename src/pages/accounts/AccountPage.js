e0422dc5a919a2593356a28c7c3bda55c23f3c60
德华 刘 <gavinprior5@gmail.com>
2023-12-06 16:29:15 +0000

    set current user & has loaded

--- src/pages/accounts/AccountPage.js
+++ src/pages/accounts/AccountPage.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import {useCurrentUser } from '../../contexts/CurrentUserContext'
 
 const AccountPage = () => {
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const currentUser = useCurrentUser();
   return (
     <div className="mt-16 h-24 bg-profile-background w-fit p-4 border-2 border-tweet-border-color ">
       <div>{/* Avatar */}</div>
