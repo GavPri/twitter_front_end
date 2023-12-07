5a6f5453b4102556f28bf8745f14a8caffc896dd
德华 刘 <gavinprior5@gmail.com>
2023-12-07 18:43:42 +0000

    deconstuct props

--- src/pages/replies/Reply.js
+++ src/pages/replies/Reply.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const Reply = (props) => {
-  return (
-    <div>Reply</div>
-  )
-}
+  const { account_id, account_image, owner, updated_at, content } = props;
+  return <div></div>;
+};
 
-export default Reply
+export default Reply;
