e2c4d22393c882cd44fc8ec13e59cca0211f6b81
德华 刘 <gavinprior5@gmail.com>
2023-12-07 10:16:03 +0000

    first of try catch in handle submit

--- src/pages/replies/ReplyFrom.js
+++ src/pages/replies/ReplyFrom.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { axiosRes } from "../../api/axiosDefaults";
 
 function ReplyFrom() {
   const [content, setContent] = useState("");
@@ -7,6 +8,17 @@ function ReplyFrom() {
   const handleChange = (event) => {
     setContent(event.target.value);
   };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axiosRes.post(`/replies/`, {
+        content, 
+        tweet,
+      });
+    } catch (err) {}
+  };
+
   return <div>ReplyFrom</div>;
 }
 
