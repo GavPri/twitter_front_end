88d28d692def64e6dbc4895154177bb9693011f4
德华 刘 <gavinprior5@gmail.com>
2024-02-26 14:59:15 +0000

    create const for changeImageToast with props

--- src/pages/posts/ChangeImageToast.js
+++ src/pages/posts/ChangeImageToast.js
@@ -1,3 +1,9 @@
+// ----- imports for custom toast
 import React, { useState} from 'react'
 import { Toast, Button } from 'react-bootstrap'
 import { AiOutlineInfoCircle } from 'react-icons/ai'
+
+// ---- toast component
+const ChangeImageToast = ({showToast, toggleToast}) => {
+    
+}
