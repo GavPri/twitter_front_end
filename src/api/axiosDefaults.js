642359b551b8e55567846550cf03157afd1adb88
德华 刘 <gavinprior5@gmail.com>
2024-11-12 14:58:57 +0000

    update baseURL for request

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://twitter-ci-4eb0a43581b7.herokuapp.com/";
+axios.defaults.baseURL = "https://not-twitter-api-d529a3237774.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
